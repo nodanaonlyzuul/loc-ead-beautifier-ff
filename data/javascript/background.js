76db5c324ccd066c8cc14437a29a4c7d0c9cc322
nodanaonlyzuul <beholdthepanda@gmail.com>
2015-03-24 20:39:02 -0400

    Using more static strings and .append() rather than .html() out of secutiry concern

--- data/javascript/background.js
+++ data/javascript/background.js
@@ -4,6 +4,8 @@ var siblingContainers     = $.merge(mayContainHeader.next('p'), mayOccurWithinHe
 var exampleHeader         = $("h3:contains('Example:'), h3:contains('Examples:')").first();
 var attributesTable       = $("h3:contains('Attributes:')").siblings('table');
 var nonLinkables          = ["#PCDATA", "EMPTY"];
+var emptyString           = "";
+var nbsp                  = "&nbsp;"
 
 // Remove noise
 $('h2:contains(EAD Elements)').remove();
@@ -14,22 +16,26 @@ $('h2').changeElementType("h1");
 // Restyle
 $('h3').css("background", "none").css("font-size", "1.25em").css("color", "#333");
 
-
 // Make element names linkable
 siblingContainers.each(function(i, paragraph){
-  var paragraph = $(paragraph);
-  var childTerms = paragraph.text().split(',');
+  var paragraph   = $(paragraph);
+  var childTerms  = paragraph.text().split(',');
   var linksToTerm = [];
 
   $.each(childTerms, function(index, childTerm) {
+    var childTerm = $.trim(childTerm);
+
     if (nonLinkables.indexOf(childTerm) == -1 ) {
-      linksToTerm.push("<a href='http://loc.gov/ead/tglib/elements/"+$.trim(childTerm)+".html'>"+childTerm+"</a>");
+      var anchorNode = $("<a>", {href: "http://loc.gov/ead/tglib/elements/"+childTerm+".html", text: childTerm} );
+      linksToTerm.push(anchorNode);
     }  else {
-      linksToTerm.push(childTerm)
+      var spanNode = $("<span>", {text: childTerm});
+      linksToTerm.push(spanNode);
     }
+    linksToTerm.push(nbsp);
   });
 
-  paragraph.html("").append(linksToTerm.join(" "));
+  paragraph.html(emptyString).append(linksToTerm);
 });
 
 // Code highlighting of examples
@@ -37,12 +43,12 @@ if (exampleHeader.length > 0) {
   var usageBlock = exampleHeader.siblings("pre");
   if (usageBlock.length > 0) {
     var exampleText = usageBlock.html();
-    usageBlock.html("");
-    usageBlock.append("<code>");
+    var codeElement = $("<code>");
+    usageBlock.html(emptyString);
 
-    var codeElement = usageBlock.children("code");
+    usageBlock.append(codeElement);
     codeElement.addClass("xml").css("font-size", "1.5em");
-    codeElement.html(exampleText);
+    codeElement.append(exampleText);
     hljs.highlightBlock(codeElement[0]);
   }
 }
